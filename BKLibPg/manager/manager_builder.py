f79f982276b95f35f19eccfdab0bd27f02144973
theleerise <elieserdce@gmail.com>
2025-08-28 18:54:35 +0200

    Mod: del import

--- BKLibPg/manager/manager_builder.py
+++ BKLibPg/manager/manager_builder.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import List, Type, Optional
 from psycopg.rows import dict_row
 from BKLibPg.manager.manager_base import ManagerBase
